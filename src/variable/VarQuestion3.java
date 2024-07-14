53c33d2eaf5f3a3dddf4de4dd0aded30e6fd59d5
limEnough <limEnough@gmail.com>
2024-07-15 00:15:06 +0900

    feat: 변수 선언 문제풀이

---
+++ src/variable/VarQuestion3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarQuestion3 {
+    public static void main(String[] args) {
+        long test = 10000000000L;
+        boolean isTest = true;
+
+        System.out.println(test);
+        System.out.println(isTest);
+    }
+}
