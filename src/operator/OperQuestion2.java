7bf827dd32467d92727258626f006a09036f35b8
limEnough <limEnough@gmail.com>
2024-07-16 23:51:26 +0900

    feat: 연산자 문제풀이

---
+++ src/operator/OperQuestion2.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperQuestion2 {
+    public static void main(String[] args) {
+        double var1 = 1.5;
+        double var2 = 2.5;
+        double var3 = 3.5;
+
+        double sum = var1 + var2 + var3;
+        double overage = sum / 3;
+
+        System.out.println(sum);
+        System.out.println(overage);
+    }
+}
