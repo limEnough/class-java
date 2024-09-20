fd8f4b490882be67a5ce60bdfad561d6f8942fab
limEnough <limEnough@gmail.com>
2024-09-21 00:26:00 +0900

    feat: 배열 - 문제풀이3 보완

--- src/array/ArrQuestion3.java
+++ src/array/ArrQuestion3.java
@@ -26,34 +26,38 @@ public class ArrQuestion3 {
             - int productCount 현재 등록된 상품의 개수를 저장할 int 변수
          */
         Scanner scanner = new Scanner(System.in);
+
+        int maxProducts = 10;
         int productCount = 0;
-        String[] productNames = new String[]{};
-        int[] productPrices = new int[]{};
+        String[] productNames = new String[10];
+        int[] productPrices = new int[10];
 
         while(true) {
-            System.out.println("1. 상품 등록 | 2. 상품 목록 | 3. 종료");
-            System.out.print("메뉴를 선택하세요: ");
+            System.out.println("1. 상품 등록 | 2. 상품 목록 | 3. 종료\n메뉴를 선택하세요:");
             int answer = scanner.nextInt();
 
             if (answer == 1) {
-                if (productCount >= 10) {
+                if (productCount >= maxProducts) {
                     System.out.println("더 이상 상품을 등록할 수 없습니다.");
+                    continue;
                 }
-                else {
-                    System.out.print("상품 이름을 입력하세요: ");
-                    productNames[productCount] = scanner.nextLine();
 
-                    System.out.print("상품 가격을 입력하세요: ");
-                    productPrices[productCount] = scanner.nextInt();
+                System.out.print("상품 이름을 입력하세요: ");
+                productNames[productCount] = scanner.next();
 
-                    productCount++;
-                }
+                System.out.print("상품 가격을 입력하세요: ");
+                productPrices[productCount] = scanner.nextInt();
+
+                productCount++;
             }
             else if (answer == 2) {
                  if (productCount == 0) {
                      System.out.println("등록된 상품이 없습니다.");
-                 } else {
-                     // TODO: 출력
+                     continue;
+                 }
+
+                 for (int i = 0; i < productCount; i++) {
+                     System.out.println(productNames[i] + ": " + productPrices[i] + "원");
                  }
             }
             else if (answer == 3) {
@@ -62,6 +66,7 @@ public class ArrQuestion3 {
             }
             else {
                 System.out.println("잘못된 값입니다. 다시 입력해주세요.");
+                continue;
             }
         }
 
